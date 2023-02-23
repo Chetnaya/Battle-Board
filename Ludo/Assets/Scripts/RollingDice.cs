ab3002f1b8d998bc8116bdda458ee356fb1bece6
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-02-23 18:38:16 +0530

    Updating Random number in the Dice

--- Ludo/Assets/Scripts/RollingDice.cs
+++ Ludo/Assets/Scripts/RollingDice.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class RollingDice : MonoBehaviour
 {
+    [SerializeField] int numberGot;
+    [SerializeField] SpriteRenderer numberedSpHolder;
     [SerializeField] Sprite[] numberedSprites;
-    
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            numberGot = Random.Range(0, 6);
+            numberedSpHolder.sprite = numberedSprites[numberGot];
+        }
+    }
+
+
 }
