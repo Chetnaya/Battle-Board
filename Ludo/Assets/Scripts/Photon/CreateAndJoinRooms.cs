194bac96b101d0ca253f143aecbda049226f297a
Chetnaya Kalyan <90476376+Chetnaya@users.noreply.github.com>
2023-03-17 11:21:26 +0530

    Completed Creating/Joining Room Functionality

--- Ludo/Assets/Scripts/Photon/CreateAndJoinRooms.cs
+++ Ludo/Assets/Scripts/Photon/CreateAndJoinRooms.cs
@@ -10,19 +10,19 @@ public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
     public TMP_InputField createInput;
     public TMP_InputField joinInput;
 
-    public void createRoom()
+    public void CreateRoom()
     {
         PhotonNetwork.CreateRoom(createInput.text);
     }
 
-    public void joinRoom()
+    public void JoinRoom()
     {
         PhotonNetwork.JoinRoom(joinInput.text);
     }
 
     public override void OnJoinedRoom()
     {
-        PhotonNetwork.LoadLevel("LobbyScene");
+        PhotonNetwork.LoadLevel("GameScene");
     }
 }
 
